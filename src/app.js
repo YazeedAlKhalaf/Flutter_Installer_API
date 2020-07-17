e22091efd8ee326033ecf06ac6a2a02c8eb491e1
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-17 21:14:05 +0300

    ➕ Add 404 JSON

--- src/app.js
+++ src/app.js
@@ -11,4 +11,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1/latest_release", latestReleaseRoute);
 
+app.use(function (req, res, next) {
+  res.status(404);
+  res.json({
+    error: {
+      message: "Not found",
+      statusCode: res.statusCode,
+    },
+  });
+});
+
 module.exports = app;
