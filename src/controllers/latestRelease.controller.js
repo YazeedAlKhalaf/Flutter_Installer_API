d9ea47671b433dc5d52eb3f9f195b8c530e00076
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-08-05 12:24:39 +0300

    ✒ Update URLs and ➕ New scripts

--- src/controllers/latestRelease.controller.js
+++ src/controllers/latestRelease.controller.js
@@ -31,6 +31,29 @@ exports.getLatestReleases = function (req, res, next) {
           macos: "https://download.jetbrains.com/idea/ideaIC-2020.1.3.dmg",
         },
       },
+      scripts: {
+        append_to_path: {
+          name: "Append To PATH",
+          download_links: {
+            windows:
+              "https://gist.githubusercontent.com/YazeedAlKhalaf/fae357096390f54a768f091a35efea7f/raw/ee399fb14b7d11256870d58664761eef5d6c1945/append-to-path.bat",
+            linux:
+              "https://gist.githubusercontent.com/YazeedAlKhalaf/2e063e344b3f3f4bb99a79c601e17a13/raw/208936d814d2e755962f6a57f0dd1b02a8ef891e/append-to-path.sh",
+            macos:
+              "https://gist.githubusercontent.com/YazeedAlKhalaf/2e063e344b3f3f4bb99a79c601e17a13/raw/208936d814d2e755962f6a57f0dd1b02a8ef891e/append-to-path-zsh.sh",
+          },
+        },
+        dist: {
+          name: "Get Distro Name Script",
+          download_links: {
+            windows: "",
+            linux:
+              "https://gist.githubusercontent.com/YazeedAlKhalaf/d03c9bda0d2e3815b819d0ebccdac2e6/raw/bf357bcf5f367e9794458aa86687c48c6a596957/dist.sh",
+            macos:
+              "https://gist.githubusercontent.com/YazeedAlKhalaf/d03c9bda0d2e3815b819d0ebccdac2e6/raw/bf357bcf5f367e9794458aa86687c48c6a596957/dist.sh",
+          },
+        },
+      },
     },
   });
 };
